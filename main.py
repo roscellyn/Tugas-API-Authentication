e3cc6b358f8f9364b65df9207d45f72250c55cfe
jacelyn <18219097@std.stei.itb.ac.id>
2021-10-17 16:26:33 +0700

    add authentication

--- main.py
+++ main.py
@@ -1,16 +1,125 @@
 import json
-from fastapi import FastAPI, HTTPException
+from datetime import datetime, timedelta
+from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from jose import JWTError, jwt
 
 with open("menu.json", "r") as read_file:
     data = json.load(read_file)
-        
-app = FastAPI()
+
+SECRET_KEY = "45e307f4d0fd7b020eae6d29c230afdabe4e5414f47ab3d1f553e92158201d9f"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+app = FastAPI(title='UTS II3160 - API Authentication',
+    description='by Jacelyn Felisha 18219097\n\nHow to use:\n1. Click Authorize then input username and password\n2. Access Token will automatically be created and expired in 30min\n3. Now you can modify the menu!')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class User(BaseModel):
+    username: str
+
+class UserInDB(User):
+    hashed_password: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+fake_users_db = {
+    "asdf": {
+        "username": "asdf",
+        "hashed_password": get_password_hash("asdf"),
+    },
+}
+
+# def fake_hash_password(password: str):
+#     return "fakehashed" + password
+
+# def fake_decode_token(token):
+#     return User(
+#         username=token + "fakedecoded")
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+@app.post("/token", response_model=Token, tags=["user authentication"])
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me", tags=["user authentication"])
+async def read_my_info(current_user: User = Depends(get_current_user)):
+    return current_user
+
 @app.get('/')
 def root():
     return{'Menu':'Item'}
 
-@app.get('/menu/{item_id}')
-async def read_menu(item_id: int):
+@app.get('/menu/{item_id}', tags=["menu"])
+async def read_menu(item_id: int, current_user: User = Depends(get_current_user)):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             return menu_item
@@ -18,15 +127,15 @@ async def read_menu(item_id: int):
         status_code=404, detail=f'Item not found'
         )
 
-@app.get('/menu')
-async def read_all_menu():
+@app.get('/menu', tags=["menu"])
+async def read_all_menu(current_user: User = Depends(get_current_user)):
     return data['menu']
     raise HTTPException(
         status_code=404, detail=f'Data not found'
         )
 
-@app.post('/menu')
-async def add_menu(name: str):
+@app.post('/menu', tags=["menu"])
+async def add_menu(name: str, current_user: User = Depends(get_current_user)):
     id=1
     if(len(data['menu'])>0):
         id=data['menu'][len(data['menu'])-1]['id']+1
@@ -45,8 +154,8 @@ async def add_menu(name: str):
         status_code=500, detail=f'Internal server error'
         )
 
-@app.put('/menu')
-async def update_menu(item_id: int, name: str):
+@app.put('/menu',  tags=["menu"])
+async def update_menu(item_id: int , name: str, current_user: User = Depends(get_current_user) ):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             menu_item['name'] = name
@@ -60,8 +169,8 @@ async def update_menu(item_id: int, name: str):
         status_code=404, detail=f'Item not found'
         )
 
-@app.delete('/menu')
-async def delete_menu(item_id: int):
+@app.delete('/menu', tags=["menu"])
+async def delete_menu(item_id: int, current_user: User = Depends(get_current_user) ):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             data['menu'].remove(menu_item)
